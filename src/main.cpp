303646e420638a50e9255f0df7d9608f52cd75c8
муен <167233615+miaeun@users.noreply.github.com>
2025-10-24 12:39:22 +0300

    feat: working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,109 @@
 #include <Arduino.h>
 
-const int ledPin = 23;
+const int ledPin = 2;
 const int relayPin = 22;
 const int ldrPin = 32;
-const int buttonPins[] = {21, 12, 14, 27};
+const int buttonPins[] = {21, 27, 26, 25};
 int secretCode[] = {1, 2, 3, 4};
 int enteredCode[4] = {0, 0, 0, 0};
 int codeIndex = 0;
-unsigned long lastDebounceTime = 0;
-const unsigned long debounceDelay = 50;
-int lastButtonState[] = {HIGH, HIGH, HIGH, HIGH};
-int buttonState[] = {HIGH, HIGH, HIGH, HIGH};
-
 bool isDoorOpen = false;
 unsigned long doorOpenTime = 0;
 const unsigned long doorOpenDuration = 10000;
-bool isBlinking = false;
 unsigned long lastBlinkTime = 0;
 const unsigned long blinkInterval = 500;
-bool codeWasReset = false;
+bool ledState = true;
+const int darkThreshold = 1000;
+bool isDark = false;
+
 void checkButtons();
-void resetCode();
+void handleButtonPress(int buttonIndex);
 void checkCode();
+void openDoor();
+void closeDoor();
+void resetCode();
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Работает");
   pinMode(ledPin, OUTPUT);
   pinMode(relayPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
   digitalWrite(ledPin, HIGH);
   digitalWrite(relayPin, LOW);
   for (int i = 0; i < 4; i++) {
     pinMode(buttonPins[i], INPUT_PULLUP);
-    lastButtonState[i] = digitalRead(buttonPins[i]);
   }
 }
+
 void loop() {
   int ldrValue = analogRead(ldrPin);
+  isDark = (ldrValue > darkThreshold);
+  static unsigned long lastLdrPrint = 0;
+  if (millis() - lastLdrPrint > 2000) {
+    Serial.print("LDR: ");
+    Serial.print(ldrValue);
+    Serial.print(", Темно: ");
+    Serial.println(isDark);
+    lastLdrPrint = millis();
+  }
   if (isDoorOpen) {
     if (millis() - doorOpenTime >= doorOpenDuration) {
-      digitalWrite(relayPin, LOW);
-      isDoorOpen = false;
-      isBlinking = false;
-      digitalWrite(ledPin, HIGH);
-      Serial.println("дверь закрыта");
+      closeDoor();
     } else {
       if (millis() - lastBlinkTime >= blinkInterval) {
         lastBlinkTime = millis();
-        digitalWrite(ledPin, !digitalRead(ledPin));
+        ledState = !ledState;
+        digitalWrite(ledPin, ledState);
       }
     }
-  }
-  if (!isDoorOpen) {
-    if (ldrValue > 2000) {
-      if (!isBlinking) {
-        digitalWrite(ledPin, HIGH);
-      }
+  } else {
+    if (isDark) {
+      digitalWrite(ledPin, HIGH);
       checkButtons();
-      codeWasReset = false;
     } else {
       digitalWrite(ledPin, LOW);
-      if (!codeWasReset) {
-        resetCode();
-        codeWasReset = true;
-      }
+      resetCode();
     }
   }
-  delay(100);
+  delay(50);
 }
 void checkButtons() {
   for (int i = 0; i < 4; i++) {
     int reading = digitalRead(buttonPins[i]);
-    if (reading != lastButtonState[i]) {
-      lastDebounceTime = millis();
-    }
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-      if (reading != buttonState[i] && reading == LOW) {
-        buttonState[i] = reading;
-        Serial.print("нажата кнопка ");
-        Serial.println(i + 1);
-        enteredCode[codeIndex] = i + 1;
-        codeIndex++;
+    if (reading == LOW) {
+      delay(50);
+      if (digitalRead(buttonPins[i]) == LOW) {
+        handleButtonPress(i);
         while (digitalRead(buttonPins[i]) == LOW) {
           delay(10);
         }
-        Serial.print("~~текущий ввод: ");
-        for (int j = 0; j < codeIndex; j++) {
-          Serial.print(enteredCode[j]);
-        }
-        Serial.println();
-        if (codeIndex >= 4) {
-          checkCode();
-        }
+        delay(50);
       }
-      buttonState[i] = reading;
     }
-    lastButtonState[i] = reading;
   }
 }
 
+void handleButtonPress(int buttonIndex) {
+  Serial.print("Нажата кнопка: ");
+  Serial.println(buttonIndex + 1);
+  enteredCode[codeIndex] = buttonIndex + 1;
+  codeIndex++;
+  Serial.print("Текущий ввод: ");
+  for (int j = 0; j < codeIndex; j++) {
+    Serial.print(enteredCode[j]);
+  }
+  Serial.println();
+  if (codeIndex >= 4) {
+    checkCode();
+  }
+}
 void checkCode() {
   Serial.print("проверка кода: ");
   for (int i = 0; i < 4; i++) {
     Serial.print(enteredCode[i]);
   }
   Serial.println();
-  
   bool correct = true;
   for (int i = 0; i < 4; i++) {
     if (enteredCode[i] != secretCode[i]) {
@@ -112,19 +111,30 @@ void checkCode() {
       break;
     }
   }
-  
   if (correct) {
-    Serial.println("дверь открыта на 10 секунд");
-    digitalWrite(relayPin, HIGH);
-    isDoorOpen = true;
-    doorOpenTime = millis();
-    isBlinking = true;
-    lastBlinkTime = millis();
+    Serial.println("верный код");
+    openDoor();
   } else {
     Serial.println("неверный код");
     resetCode();
   }
 }
+
+void openDoor() {
+  isDoorOpen = true;
+  doorOpenTime = millis();
+  digitalWrite(relayPin, HIGH);
+  Serial.println("Реле включено");
+  resetCode();
+}
+
+void closeDoor() {
+  isDoorOpen = false;
+  digitalWrite(relayPin, LOW);
+  digitalWrite(ledPin, HIGH);
+  Serial.println("Реле вЫключено, дверь закрыта");
+}
+
 void resetCode() {
   if (codeIndex > 0) {
     codeIndex = 0;
